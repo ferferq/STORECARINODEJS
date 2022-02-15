277c19e35fe952e23b62118ef7c14ccd158cd638
fmartins <fernandoalvesq@gmail.com>
2022-02-15 08:02:18 -0300

    Criacao do provider da s3

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,6 +5,7 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StoreProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>(
@@ -19,5 +20,5 @@ container.registerInstance<IMailProvider>(
 
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  LocalStorageProvider
+  S3StoreProvider
 );
